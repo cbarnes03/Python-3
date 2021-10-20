e89a75962ff3bc6380ecd927acac6be91e3b30ec
Charles Barnes <onthisrock97@gmail.com>
2021-10-19 22:52:32 -0400

    This commit marks to my return to learning python 3 via code academy. I added my steps along lesson 1 as well as the ASCII project & the Store list.

---
+++ ASCIIPythonProject.py
@@ -0,0 +1,22 @@
+# Charles Barnes
+#Fun Fact: I love skiing
+first_name1 = """ 
+ CCC    H   H     A    RRRRR   L      EEEEE  SSSSS
+C   C   H   H    A A   R   R   L      E      S
+C       H   H   A   A  R   R   L      E      S
+C       HHHHH   AAAAA  RRRR    L      EEEEE  SSSSS
+C       H   H   A   A  R R     L      E          S
+C    C  H   H   A   A  R  R    L      E          S
+ CCC    H   H   A   A  R    R  LLLLL  EEEEE  SSSSS
+ """
+ 
+last_name1 = """
+BBBB     A    RRRR   N   N  EEEEE  SSSSS
+B   B   A A   R   R  NN  N  E      S
+B   B  A   A  R   R  N N N  E      S
+BBBB   AAAAA  RRRR   N  NN  EEEEE  SSSSS
+B   B  A   A  R R    N   N  E          S
+B   B  A   A  R  R   N   N  E          S
+BBBB   A   A  R   R  N   N  EEEEE  SSSSS
+"""
+print(first_name1 + last_name1)
---
+++ CodeAcademyPythonLesson1.py
@@ -0,0 +1,131 @@
+# This is the python code that represents my journey in baby steps in codeacademy
+# First lesson Part 1) Hello World
+my_name = "Charlie"
+print("Hello and welcome " + my_name + "!")
+
+# Part 2) Comments:
+# This is a comment
+
+# Part 3) Print
+print( "Hello World!")
+
+#Part 4) Strings
+print("Charlie")
+print('Charlie')
+
+#Part 5) Variables
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "Chopped Cheese with everything & Chipotle Mayo"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update "meal" to be dinner
+meal = "Buffalo Chicken Salad with spinach & hard boiled-eggs"
+# Printing out dinner
+print("Dinner:")
+print(meal)
+
+#Part 6) Errors
+print('This message has mismatched quote marks!")
+print(Abracadabra)
+#CORRECTED:
+print("This message has mismatched quote marks!")
+print("Abracadabra")
+#Part 7) Numbers
+# Define the release and runtime integer variables below:
+release_year = 2021
+runtime =  117
+
+# Define the rating_out_of_10 float variable below: 
+
+rating_out_of_10 = 9.6
+
+#Part 8) Calculations
+pemdas = 25 * 68 + 13 / 28
+print(pemdas)
+#Part 10) Changing Numbers
+quilt_width = 8
+quilt_length = 12
+quilt = quilt_width * quilt_length
+print(quilt)
+quilt_length = 8
+quilt = quilt_width * quilt_length
+print(quilt)
+
+#Part 9) Exponents
+# Calculation of squares for:
+# 6x6 quilt
+quilt_1 = 6 ** 2
+print(quilt_1)
+# 7x7 quilt
+quilt_2 = 7 ** 2
+print(quilt_2)
+# 8x8 quilt
+quilt_3 = 8 ** 2
+print(quilt_3)
+# How many squares for 6 people to have 6 quilts each that are 6x6?
+answer = 6 ** 4
+print(answer)
+
+Part 10) Modulo
+my_team = 27 % 4
+print(my_team)
+his_team = 26 % 4
+her_team = 28 % 4
+print(his_team)
+print(her_team)
+
+#Part 11) Concatentation
+string1 = "The wind, "
+string2 = "which had hitherto carried us along with amazing rapidity, "
+string3 = "sank at sunset to a light breeze; "
+string4 = "the soft air just ruffled the water and "
+string5 = "caused a pleasant motion among the trees as we approached the shore, "
+string6 = "from which it wafted the most delightful scent of flowers and hay."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+#print(message)
+print(message)
+
+#Part 12) Plus Equals
+total_price = 0
+
+new_sneakers = 50.00
+
+total_price += new_sneakers
+
+nice_sweater = 39.00
+fun_books = 20.00
+# Update total_price here:
+total_price += nice_sweater
+total_price += fun_books
+print("The total price is", total_price)
+
+#Part 13) Multi-line strings
+# Assign the string here
+to_you = """Stranger, if you passing meet me and desire to speak to me,
+why
+  should you not speak to me?
+And why should I not speak to you?"""
+
+
+print(to_you)
+
+#Part 14) Review
+my_age = 24
+half_my_age = 24/2
+greeting = "Hello there, General "
+name = "Charles"
+greeting_with_name = (greeting + name)
+print(greeting_with_name)
