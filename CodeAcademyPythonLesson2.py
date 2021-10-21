e91c175ca4051a295d8c8b6c857150def74fe3b2
Charles Barnes <onthisrock97@gmail.com>
2021-10-21 13:31:32 -0400

    This is the beginning of Lesson 2 Control Flow on Code Academy.

---
+++ CodeAcademyPythonLesson2.py
@@ -0,0 +1,51 @@
+#Lesson 2 Control Flow
+#Part 1) Boolean Expressions, identify which statement is a boolean expression by yes if so, and no if not.
+#ExS:My dog is the cutest dog in the world.
+example_statement = "No"
+#1S:Dogs are mammals.
+statement_one = "Yes"
+#2S:My dog is named Pavel.
+statement_two = "Yes"
+#3S:Dogs make the best pets.
+statement_three = "No"
+#4S:Cats are female dogs.
+statement_four = "Yes"
+
+#Part 2) Relational Operators: Equals and Not Equals
+#1S: (5 * 2) - 1 == 8 + 1
+statement_one = True
+#2S: 13 - 6 != (3 * 2) + 1
+statement_two = False
+#3S: 3 * (2 - 1) == 4 - 1
+statement_three = True
+
+#Part 3) Boolean Variables
+my_baby_bool = "true"
+print(type(my_baby_bool))		#this is a string
+my_baby_bool_two = True
+print(type(my_baby_bool_two))	#this is a boolean
+
+#Part 4) If Statement
+# Enter a user name here, make sure to make it a string
+user_name = "Dave"
+user_name = "angela_catlady_87"
+
+if user_name == "Dave":
+  print("Get off my computer Dave!")
+
+if user_name == "angela_catlady_87":
+  print("I know it is you, Dave! Go away!")
+
+#Part 5)
+
+#Part 6)
+
+#Part 7)
+
+#Part 8)
+
+#Part 9)
+
+#Part 10)
+
+#Part 11)
