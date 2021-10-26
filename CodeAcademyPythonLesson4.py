1b522ea695918be8adeaebec2c96b7c77ef7bd0f
Charles Barnes <onthisrock97@gmail.com>
2021-10-26 19:32:56 -0400

    This python program will detail further methods for interacting with lists.

---
+++ CodeAcademyPythonLesson4.py
@@ -0,0 +1,15 @@
+#CodeAcademyPythonLesson4.py 
+# Working with Lists
+#Part 1) Adding by Index: Insert
+
+#Part 2) 
+#Part 3)
+#Part 4)  
+#Part 5) 
+#Part 6) 
+#Part 7) 
+#Part 8) 
+#Part 9) 
+#Part 10) 
+#Part 11) 
+
