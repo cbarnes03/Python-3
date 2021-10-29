4e8bbfafbddaa3b6ff6cf9fb3b94922a0519c4f1
Charles Barnes <onthisrock97@gmail.com>
2021-10-29 14:44:01 -0400

    This file represents my progress through the Loop lesson in Codeacademy.

---
+++ CodeAcademyPythonLesson5.py
@@ -0,0 +1,37 @@
+#CodeAcademyPythonLesson5.py
+#Lesson 5 Loops
+#Part 1)Why Loops?
+# Write 10 print() statements below! 
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+print("This can be so much easier with loops!")
+
+#Part 2)For Loops: Introduction
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+    print(game) 
+
+for sport in sport_games:
+    print(sport)
+	
+#Part 3)For Loops: Using Range
+
+#Part 4)
+#Part 5)
+#Part 6)
+#Part 7)
+#Part 8)
+#Part 9)
+#Part 10)
+#Part 11)
+#Part 12)
