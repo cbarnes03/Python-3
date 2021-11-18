cb18b0788ee23101835f56665e06deccecaefe7c
Charles Barnes <onthisrock97@gmail.com>
2021-11-18 12:53:27 -0500

    This commit marks more progress in the python section regarding Loops while control mechanisms including break and continue.

--- CodeAcademyPythonLesson5.py
+++ CodeAcademyPythonLesson5.py
@@ -30,9 +30,51 @@ promise = "I will finish the python loops module!"
 for lines in range(5):
     print(promise)
 #Part 4)While Loops: Introduction
+# # While Loop Walkthrough
+# count = 0
+# print("Starting While Loop")
+# while count <= 3:
+#   # Loop Body
+#   # Print if the condition is still true
+#   print("Loop Iteration - count <= 3 is still true")
+#   # Print the current value of count 
+#   print("Count is currently " + str(count))
+#   # Increment count
+#   count += 1
+#   print(" ----- ")
+# print("While Loop ended")
 
-#Part 5)
-#Part 6)
+# # Your code below: 
+
+countdown = 10
+while countdown >= 0:
+  print(countdown)
+  countdown -= 1
+print("We have liftoff!")
+# or the one-line version:
+countdown = 10
+while countdown >= 0: print(countdown); countdown -= 1
+print("We have liftoff!")
+#Part 5)While Loops: Lists
+python_topics = ["variables", "control flow", "loops", "modules", "classes"]
+
+#Your code below: 
+length = len(python_topics)
+index = 0
+while index < length:
+  print("I am learning about " + python_topics[index] + ".")
+  index += 1
+#Part 6) Loop Control: Break
+dog_breeds_available_for_adoption = ["french_bulldog", "dalmatian", "shihtzu", "poodle", "collie"]
+dog_breed_I_want = "dalmatian"
+
+for dog_breed in dog_breeds_available_for_adoption:
+  print(dog_breed)
+  if dog_breed == dog_breed_I_want:
+    print("They have the dog I want!")
+    break
+  
+    
 #Part 7)
 #Part 8)
 #Part 9)
