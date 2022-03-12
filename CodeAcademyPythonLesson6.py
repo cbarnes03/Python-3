6af73d9e1d681a9ba93cf5ff193d5dc534c6fc44
Charles Barnes <onthisrock97@gmail.com>
2022-03-11 19:37:35 -0500

    For demonstration I added a + in one of the comments.

--- CodeAcademyPythonLesson6.py
+++ CodeAcademyPythonLesson6.py
@@ -6,7 +6,7 @@ print("Setting the Empire State Building as the starting point and Times Square 
 print("Calculating the total distance between our points.") 
 print("The best route is by train and will take approximately 10 minutes.") 
 
-# Second user wants to travel between these two points!
+# Second user wants to travel between these two points! + 
 print("Setting the Empire State Building as the starting point and Times Square as our destination.")
 print("Calculating the total distance between our points.") 
 print("The best route is by train and will take approximately 10 minutes.")
